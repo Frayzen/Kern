9654bdca030de520445217673d0396be88ae2bab
florian.laine <florian.laine@epita.fr>
2024-06-15 22:57:47 +0200

    wip: int handler

--- k/interrupts/handler.c
+++ k/interrupts/handler.c
@@ -1,11 +1,22 @@
 #include "handler.h"
 #include "serial.h"
 
-void interrupt_handler(stack* stack)
+void interrupt_handler(stack *stack)
 {
-    println("Interrupt handler called");
-    print_uint(stack->eflags, 4);
-    print_uint(stack->err_code, 2);
-    print_uint(stack->int_no, 1);
+	println("Interrupt handler called");
+	print("Stack:");
+	print_uint(stack->int_no, 2);
+	print(" Error code:");
+	print_uint(stack->err_code, 2);
+	print(" EFLAGS:");
+	print_uint(stack->eflags, 4);
+	print(" EIP:");
+	print_uint(stack->eip, 4);
+	print(" CS:");
+	print_uint(stack->cs, 4);
+	print(" EFLAGS:");
+	print_uint(stack->eflags, 4);
+	print(" EBP:");
+	print_uint(stack->ebp, 8);
     println();
 }

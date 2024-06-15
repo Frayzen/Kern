7a23df6fe0f5d777974073ba01772aec67de16e3
florian.laine <florian.laine@epita.fr>
2024-06-15 17:40:43 +0200

    feat: tss values

--- k/gdt/tss.c
+++ k/gdt/tss.c
@@ -1,8 +1,14 @@
 #include "tss.h"
 
-static tss_entry tss;
+static tss_entry tss = {0};
 
 tss_entry* setup_tss()
 {
+    tss.esp0 = 0xFFFFF;
+    tss.ss0 = 0x20;
+    tss.esp1 = 0xFFFFF;
+    tss.ss1 = 0x20;
+    tss.esp2 = 0xFFFFF;
+    tss.ss2 = 0x20;
     return &tss;
 }
